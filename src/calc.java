6a9d10aaca4e8bef8df8f9a83c8284b2b143fdc7
KamSzym <kamuda.sz@gmail.com>
2017-05-11 13:20:57 +0200

    basic things works just fine

--- src/calc.java
+++ src/calc.java
@@ -1,34 +1,36 @@
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class calc{
-	
+	modeSelection mode;
 	JFrame frame1;
 	JButton numButtons[], mrcBut, mPlusBut, mMinusBut, sqrtBut, percentBut, 
 			dotBut, cBut, isEqualBut, addBut, subtractBut, divideBut, multipleBut;
 	JTextArea textArea;
 	
+	String inputString;
+	Double num1, num2, num2Old;
+	
 	
 	public static void main(String [] args){
+		
 		System.out.println("test nr 2");
-		new calc();
-		
-		
+		calc calcFrame =new calc();
+		calcFrame.frameInit();		
 	}
 	
 	public calc(){
+		
+		mode=modeSelection.NOTHING;
+		inputString="";
+		num1=(double) 0;
+		num2=(double)0;
 		frame1=new JFrame();
-		frame1.setSize(400,300);
-		frame1.setLocationRelativeTo(null);
-		frame1.setTitle("SKCalc");
-		
-		JPanel testPanel=new JPanel();
-		JPanel panel1=new JPanel(new GridBagLayout());
-		GridBagConstraints c=new GridBagConstraints();
-		
 		
 		numButtons=new JButton[10];
 		for(int i=0;i<10;i++){
@@ -49,20 +51,35 @@ public class calc{
 		divideBut=new JButton("/");
 		multipleBut=new JButton("*");
 		
-		textArea=new JTextArea("testing, no logic jet");
-		c.fill=(GridBagConstraints.BOTH);
+		textArea=new JTextArea("jebać stare kurwy pdk");
 		
-		c.weighty=100000;
-		c.weightx=1000000;
+		
+	}
+	public void frameInit(){
+		
+		butListener butPressed=new butListener();
+		for(int i=0;i<10;i++){
+			
+			numButtons[i].addActionListener(butPressed);
+		}
+		
+		frame1.setSize(400,300);
+		frame1.setLocationRelativeTo(null);
+		frame1.setTitle("socjetChan.gov");
+		
+		JPanel panel1=new JPanel(new GridBagLayout());
+		GridBagConstraints c=new GridBagConstraints();
+		
+		c.fill=(GridBagConstraints.BOTH);
+		c.weighty=1;
+		c.weightx=1;
 		c.insets=new Insets(5,5,5,5);
 		
-		c.gridx=0; c.gridy=0;
-		c.gridwidth=4;
+		c.gridx=0; c.gridy=0; c.gridwidth=4;
 		panel1.add(textArea, c);
-		c.gridwidth=1;
-		
-		c.gridx=0; c.gridy=1;
+		c.gridwidth=1; c.gridx=0; c.gridy=1;
 		panel1.add(mrcBut, c);
+		
 		c.gridx=1; panel1.add( mMinusBut,c);
 		c.gridx++; panel1.add( mPlusBut,c);
 		c.gridx++; panel1.add( sqrtBut,c);
@@ -81,30 +98,163 @@ public class calc{
 		c.gridy+=2;
 		panel1.add(isEqualBut,c);
 		c.gridheight=1;
-		
-		
-		
-		
-		
-		
-		
-
-		
-		
 
 		for(int y=0;y<3;y++){
+			
 			c.gridy=y+2;
 			for(int x=0;x<3;x++){
-				c.gridx=x;
-				panel1.add(numButtons[y*3+x+1],c);
 				
+				c.gridx=x;
+				panel1.add(numButtons[y*3+x+1],c);	
 			}
 		}
-		
 		
 		frame1.add(panel1);
 		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame1.setVisible(true);
+		addBut.addActionListener(butPressed);
+		subtractBut.addActionListener(butPressed);
+		multipleBut.addActionListener(butPressed);
+		divideBut.addActionListener(butPressed);
+		dotBut.addActionListener(butPressed);
+		isEqualBut.addActionListener(butPressed);
+		sqrtBut.addActionListener(butPressed);
+		cBut.addActionListener(butPressed);
 		
+	}
+	
+	private void keyAction(final JButton button)throws NumberFormatException{
+
+		if(button==addBut){
+			makeAction(modeSelection.ADD);
+		}else if(button==subtractBut){
+			makeAction(modeSelection.SUBTRACT);
+		}else if(button==isEqualBut){
+			makeAction(modeSelection.ISEQUAL);
+		}else if(button==cBut)
+		{
+			makeAction(modeSelection.CLEAR);			
+		}else if(button==multipleBut){
+			makeAction(modeSelection.MULTIPLY);
+			
+		}else if(button==divideBut){
+			makeAction(modeSelection.DIVIDE);
+			
+		}else if(button==sqrtBut){
+			makeAction(modeSelection.SQRT);
+			
+		}else{
+			inputString+=button.getText();
+			textArea.setText(inputString);
+		}
+		
+	}
+	public void makeAction(modeSelection newMode){
+		
+		if(inputString.startsWith("") || inputString.charAt(0)=='.'){
+			inputString="0"+inputString;
+		}
+		
+		
+		if(newMode==modeSelection.CLEAR)
+		{
+			num1=(double) 0;
+			num2=(double) 0;
+			inputString=""; textArea.setText(num2.toString());
+			newMode=modeSelection.NOTHING;
+			
+		}else if(newMode==modeSelection.SQRT)
+		{
+			num1=Double.parseDouble(inputString);
+			if(num1!=0)
+			{
+				num2Old=num1;
+				num2=Math.sqrt(num1);
+				
+			}
+			else
+			{
+				num2Old=num2;
+				num2=Math.sqrt(num2);
+				
+			}
+			
+			inputString=""; textArea.setText("sqrt("+num2Old+") = "+num2.toString());
+			newMode=modeSelection.ISEQUAL;
+		}
+		
+		
+		
+		
+		else if(mode==modeSelection.ADD){
+			num1=Double.parseDouble(inputString);
+			num2Old=num2;
+			num2+=num1;
+			inputString=""; textArea.setText(num2Old.toString()+" + "+ num1.toString()+" = "+num2.toString());
+		}else if(mode==modeSelection.SUBTRACT)
+		{
+			num1=Double.parseDouble(inputString);
+			num2Old=num2;
+			num2-=num1;
+			inputString=""; textArea.setText(num2Old.toString()+" - "+ num1.toString()+" = "+num2.toString());
+			
+		}else if(mode==modeSelection.NOTHING)
+		{
+			num1=Double.parseDouble(inputString);
+			num2=num1;
+			inputString=""; textArea.setText(num2.toString());
+			
+		}else if(mode==modeSelection.ISEQUAL)
+		{
+			num1=Double.parseDouble(inputString);
+			if(num1!=0){
+				num2=num1;
+			}
+			inputString=""; textArea.setText(num2.toString());
+			
+		}else if(mode==modeSelection.MULTIPLY)
+		{
+			num1=Double.parseDouble(inputString);
+			num2Old=num2;
+			num2=num2*num1;
+			inputString=""; textArea.setText(num2Old.toString()+" * "+ num1.toString()+" = "+num2.toString());
+			
+		}
+		else if(mode==modeSelection.DIVIDE)
+		{
+			num1=Double.parseDouble(inputString);
+			num2Old=num2;
+			num2=num2/num1;
+			inputString=""; textArea.setText(num2Old.toString()+" / "+ num1.toString()+" = "+num2.toString());
+			
+		}
+		
+		
+		mode=newMode;
+	}
+	
+	
+	private class butListener implements ActionListener{
+
+		@Override
+		
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			try{
+			
+				keyAction((JButton)e.getSource());
+			}catch (NumberFormatException exception){
+			
+				String message="Sorry error ocured!\nYou have propably done something wrong\nError message:  "
+							+exception.getMessage();
+				JOptionPane.showMessageDialog(frame1, message, "error", JOptionPane.ERROR_MESSAGE);
+			}
+			
+			
+		}
+		
+	}
+	public enum modeSelection{
+		ADD, SUBTRACT, DIVIDE, MULTIPLY, NOTHING, ISEQUAL, CLEAR, SQRT
 	}
 }
